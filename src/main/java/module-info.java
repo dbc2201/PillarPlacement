08ca1c34ef8c4d1e409e5eb37f06dff252697555
dbc2201 <divyanshb22@gmail.com>
2022-02-18 21:45:32 +0530

    added library for log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module io.github.dbc.pillarplacement {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.apache.logging.log4j;
 
     opens io.github.dbc.pillarplacement to javafx.fxml;
     exports io.github.dbc.pillarplacement;
